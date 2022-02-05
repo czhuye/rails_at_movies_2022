478879c073fe7f144de5de59546da4e5a0200d1f
Zhuye <z.che@outlook.com>
2022-02-05 16:39:22 -0600

    Modified the seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,9 +19,24 @@ filename = Rails.root.join("db/top_movies.csv")
 csv_data = File.read(filename)
 movies = CSV.parse(csv_data, headers: true, encoding: "utf-8")
 
-movies.each do |movie|
+movies.each do |m|
   # puts movie["original_title"]
-  ProductionCompany.create(name: ["production_company"])
+  production_company = ProductionCompany.find_or_create_by(name: m["production_company"])
+
+  if production_company && production_company.valid?
+    # create a new movie
+    movie = production_company.movies.create(
+      title:        m["original_title"],
+      year:         m["year"],
+      duration:     m["duration"],
+      description:  m["description"],
+      average_vote: m["avg_vote"]
+    )
+    puts "Invalid move #{m['original_title']}" unless movie&.valid?
+  else
+    puts "invalid production company #{m['production_company']} for movie #{m['original_title]']}"
+  end
 end
 
 puts "Created #{ProductionCompany.count} ProductionCompanies"
+puts "Created #{Movie.count} Movies"
