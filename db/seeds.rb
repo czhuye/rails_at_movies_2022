ed499600aed8055bb7c8badd9aba8c33cc27a122
Zhuye <z.che@outlook.com>
2022-02-05 07:28:33 -0600

    Second Commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "csv"
+
+Movie.delete_all
+ProductionCompany.delete_all
+
+# add the file path of the  csv file
+filename = Rails.root.join("db/top_movies.csv")
+
+# puts "Loading Movies from the CSV file: #{filename}"
+
+csv_data = File.read(filename)
+movies = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+movies.each do |movie|
+  # puts movie["original_title"]
+  ProductionCompany.create(name: ["production_company"])
+end
+
+puts "Created #{ProductionCompany.count} ProductionCompanies"
