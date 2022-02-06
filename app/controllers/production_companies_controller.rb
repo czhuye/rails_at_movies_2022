f3c49d108f36d7c68ef274d1e11698d84320eefd
Zhuye <z.che@outlook.com>
2022-02-05 18:42:41 -0600

    Add some bulma style

--- app/controllers/production_companies_controller.rb
+++ app/controllers/production_companies_controller.rb
@@ -1,7 +1,10 @@
 class ProductionCompaniesController < ApplicationController
   def index
+    # fetch all production companies
+    @production_companies = ProductionCompany.all
   end
 
   def show
+    @production_company = ProductionCompany.find(params[:id])
   end
 end
